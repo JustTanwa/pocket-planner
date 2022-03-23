4365941f3a93a0f82cfcfc117b0e9c4815c6c2be
Katakung95 <tanwasripan@hotmail.co.uk>
2022-03-23 13:49:04 +0000

    change firebase config to hide api key

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyAwfZvoqP6JfxqeaWOcS9qgAygO1kO-z2g",
-    authDomain: "pocketplanner-dev.firebaseapp.com",
-    projectId: "pocketplanner-dev",
-    storageBucket: "pocketplanner-dev.appspot.com",
-    messagingSenderId: "12138294616",
-    appId: "1:12138294616:web:39ddea08504d5561cf092d"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 });
 
 export const auth = app.auth();
